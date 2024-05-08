6c540b45e04120fe73c43e6ace04c4617bac4520
Neel <neel.pendharkar@gmail.com>
2024-05-08 20:14:37 +0530

    Added code

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 from module1.file1 import Test1
 from module1.file2 import Test2
 from module2.file3 import Test3
+from module2.file4 import Test4
 test1 = Test1()
 test2 = Test2()
 test3 = Test3()
+test4 = Test4()
---
+++ src/module2/file4.py
@@ -0,0 +1,3 @@
+class Test4:
+    def __init__(self):
+        print('Test4 initialised new code')
